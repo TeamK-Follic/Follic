6c4c04d8f93c0effba670f7f0b901a1cc6ddb9b6
solaretech <solaretech.kk@gmail.com>
2019-01-20 14:21:35 +0000

    [create]genre new view & controller

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,17 @@
+class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+    genre.save
+    redirect_to artists_path
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
