895991a1483a9a0d06407c8f4a43835573a900b8
Yuri <yu5kxk.g@gmail.com>
2019-01-29 08:30:53 +0000

    [updade]CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authenticate_manager!, only: [:index]
-  before_action :authenticate_user!, only: [:show, :index, :edit, :update]
-  before_action :ensure_login_user, only: [:show, :edit, :update]
+  # before_action :authenticate_manager!, only: [:index]
+  # before_action :authenticate_user!, only: [:show, :index, :edit, :update]
+  # before_action :ensure_login_user, only: [:show, :edit, :update]
 
   def about
   end
